5a60c6a39f68e68426ba61c0dbf1c225286f8cb3
Saad Akllam <sakllam@e2r7p2.1337.ma>
2023-04-09 12:32:04 +0000

    it works now

--- Data/switch.sh
+++ Data/switch.sh
@@ -1,4 +1,4 @@
+newName="newName"
 oldName=".git"
-newName="NotGit"
 
-find /home/sakllam/Desktop/ft_transcendence/Data/*/*/* -type d -name "$oldName" -execdir mv {} "newName" \;
+find /goinfre/sakllam/ft_transcendence/Data/*/*/* -type d -name "$oldName" -execdir mv {} "$newName" \;
