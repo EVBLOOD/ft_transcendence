9a3134eeafda8d115aefc66cccf4cffd493d4cc9
sama-sama <test@test.com>
2023-04-30 14:49:20 +0100

    adding swagger

--- Data/back/back/src/main.ts
+++ Data/back/back/src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('transcendence')
+    .setDescription('transcendence APIs description')
+    .setVersion('0.1')
+    .addTag('transcendence')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.use(session({
     secret: "isktcha?",
     resave: false,
