55845aa5b35834e53f9f56240b193ec180753bae
saad akllam <saad.akllam@gmail.com>
2023-05-06 22:49:46 +0100

    tasks for tomorow are defined

--- Data/back/back/src/chat/chat.gateway.ts
+++ Data/back/back/src/chat/chat.gateway.ts
@@ -34,18 +34,22 @@
 // }
 
 
-import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, OnGatewayConnection } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, ConnectedSocket } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
-import { UpdateChatDto } from './dto/update-chat.dto';
+// import { UpdateChatDto } from './dto/update-chat.dto';
 import { Server, Socket } from 'socket.io'
+import { UseGuards } from '@nestjs/common';
+import { IsauthGuard } from 'src/auth/isauth.guard';
 // import { AuthService } from 'src/auth/auth.service';
 
+// cors origins
 @WebSocketGateway({namespace: 'chat'})
-export class ChatGateway implements OnGatewayConnection {
+export class ChatGateway {
   constructor(private readonly chatService: ChatService) {} //  to check if a user exist -> yoou will have to enable the second constractor.. parse data and send it to auth to check if the user is logged or valid
   // constructor(private readonly chatService: ChatService, private readonly auth: AuthService) {} // before this I'll have to test Guards
 
+  // @UseGuards(IsauthGuard)
   @WebSocketServer()
   server: Server;
 
@@ -54,12 +58,16 @@ export class ChatGateway implements OnGatewayConnection {
     console.log("init Connection");
     return client.id;
   }
+  // @UseGuards(IsauthGuard)
   @SubscribeMessage('createChat')
-  createChat(@MessageBody() body: string) : string {
+  createChat(@MessageBody() body: string, @ConnectedSocket() client: Socket) : string {
     console.log("createChat : called");
     console.log(body);    // this.server.send("hello from the server ;")
+    console.log(client);
     // Server.c
+    client.emit('createChat', "emit? was reieved");
     return "body was reieved";
+    // await this.server.in(payload.user.socketId).socketsJoin(payload.roomName)
     // return this.chatService.create(createChatDto);
   }
 
@@ -73,13 +81,15 @@ export class ChatGateway implements OnGatewayConnection {
     // return this.chatService.findOne(id);
   }
 
-  @SubscribeMessage('updateChat')
-  update(@MessageBody() updateChatDto: UpdateChatDto) {
-    // return this.chatService.update(updateChatDto.id, updateChatDto);
-  }
+  // @SubscribeMessage('updateChat')
+  // update(@MessageBody() updateChatDto: UpdateChatDto) {
+  //   // return this.chatService.update(updateChatDto.id, updateChatDto);
+  // }
 
   @SubscribeMessage('removeChat')
   remove(@MessageBody() id: number) {
     // return this.chatService.remove(id);
   }
+  // sync handleDisconnect(socket: Socket)
+  // handleSetClientDataEvent
 }
--- Data/back/back/src/main.ts
+++ Data/back/back/src/main.ts
@@ -6,9 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['log', 'error', 'warn', 'debug'],
-  });
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('transcendence')
