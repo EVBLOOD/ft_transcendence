6e8cded1c38e7ff7f14f9eb32884b320c80e822f
saad akllam <saad.akllam@gmail.com>
2023-06-10 19:51:18 +0100

    getting start with upload file for avatars with plans of saving them localy

--- Data/back/back/src/user/user.controller.ts
+++ Data/back/back/src/user/user.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Body, Param, Put, UseGuards } from '@nestjs/common';
+import { Controller, Get, Body, Param, Put, UseGuards, Post, UseInterceptors, UploadedFile, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/authenticator/jwtauth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @UseGuards(JwtAuthGuard)
 @Controller('user')
@@ -23,4 +25,30 @@ export class UserController {
     return await this.userService.update(id, updateUserDto);
   }
 
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploadedFiles/avatars'
+    })
+  }))
+  upload(@UploadedFile() file: Express.Multer.File, @Res() res)
+  {
+    console.log("..")
+    if (file.mimetype != "image/jpeg" && file.mimetype != "image/png")
+      return res.status(418).send("file is not a suported image!");
+    if (file.size > 2 * 1024 * 1024) // 2M
+      return res.status(413).send("file too large!");
+      // {
+      //   path: file.path,
+      //   filename: file.originalname,
+      //   mimetype: file.mimetype
+      // }
+      // private localFilesService: LocalFilesService
+      // const avatar = await this.localFilesService.saveLocalFileData(fileData);
+    // now we will create the file and store it's path.
+    // console.log(file);
+    // if (file.)
+    return file.buffer;
+  }
 }
