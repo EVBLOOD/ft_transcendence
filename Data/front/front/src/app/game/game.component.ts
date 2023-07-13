fc1810f6e47c21accb3ee51ca1181eaab6ef5f9c
EVBLOOD <saad.akllam@gmail.com>
2023-07-13 09:24:13 +0100

    game - leaderboard

--- Data/front/front/src/app/game/game.component.ts
+++ Data/front/front/src/app/game/game.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
+  players = [
+    { img:'/assets/img/profile.jpeg', name: 'sakllam', status: 1},
+    { img:'/assets/img/profile.jpeg', name: 'kid-bouh', status: 1},
+    { img:'/assets/img/profile.jpeg', name: 'ahmed', status: 0},
+    { img:'/assets/img/profile.jpeg', name: 'yamzil', status: 1},
+    { img:'/assets/img/profile.jpeg', name: 'karim', status: 1},
+    { img:'/assets/img/profile.jpeg', name: 'test', status: 0},
+  ];
 
+  leaderboard = [
+    { img:'/assets/img/profile.jpeg', name: 'kid-bouh', rank: '/assets/icons/firstRank.svg'},
+    { img:'/assets/img/profile.jpeg', name: 'yamzil', rank: '/assets/icons/firstRank.svg'},
+    { img:'/assets/img/profile.jpeg', name: 'sakllam', rank: '/assets/icons/firstRank.svg'},
+    { img:'/assets/img/profile.jpeg', name: 'eagoumi', rank: '/assets/icons/firstRank.svg'},
+    { img:'/assets/img/profile.jpeg', name: 'imabid', rank: '/assets/icons/firstRank.svg'},
+  ];
 }
--- Data/front/front/src/app/leaderbord/leaderbord.component.ts
+++ Data/front/front/src/app/leaderbord/leaderbord.component.ts
@@ -17,9 +17,17 @@ export class LeaderbordComponent {
 
   leaderboard = [
     { img:'/assets/images/profile.jpg', name: 'kid-bouh', rank: '/assets/icons/rank1.svg'},
-    { img:'/assets/images/profile.jpg', name: 'yamzil', rank: '/assets/icons/rank1.svg'},
-    { img:'/assets/images/profile.jpg', name: 'sakllam', rank: '/assets/icons/rank1.svg'},
-    { img:'/assets/images/profile.jpg', name: 'eagoumi', rank: '/assets/icons/rank1.svg'},
-    { img:'/assets/images/profile.jpg', name: 'imabid', rank: '/assets/icons/rank1.svg'},
+    { img:'/assets/images/profile.jpg', name: 'yamzil', rank: '/assets/icons/rank2.svg'},
+    { img:'/assets/images/profile.jpg', name: 'sakllam', rank: '/assets/icons/rank3.svg'},
+    { img:'/assets/images/profile.jpg', name: 'eagoumi', rank: '/assets/icons/rank4.svg'},
+    { img:'/assets/images/profile.jpg', name: 'imabid', rank: '/assets/icons/rank5.svg'},
+  ];
+
+  history = [
+    { img1:'/assets/images/profile.jpg', img2:'/assets/simages/profile.jpg', names: 'kid-bouh vs yamzil', state: 'Win 🏆', date: 'Today'},
+    { img1:'/assets/images/profile.jpg', img2:'/assets/images/profile.jpg', names: 'kid-bouh vs agoumiiiiii', state: 'Win 🏆', date: 'Today'},
+    { img1:'/assets/images/profile.jpg', img2:'/assets/images/profile.jpg', names: 'kid-bouh vs simo', state: 'Lose 😥', date: 'Yesterday'},
+    { img1:'/assets/images/profile.jpg', img2:'/assets/images/profile.jpg', names: 'kid-bouh vs test', state: 'Lose 😥', date: 'Yesterday'},
+    { img1:'/assets/images/profile.jpg', img2:'/assets/images/profile.jpg', names: 'kid-bouh vs sel-mars', state: 'Win 🏆', date: 'Yesterday'},
   ];
 }
