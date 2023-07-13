84a013a8bc6d8026086ee14ecf1b5265b20b47a5
saad akllam <saad.akllam@gmail.com>
2023-07-13 17:27:48 +0100

    game waiting interface is waiting for back mergin

--- Data/front/front/src/app/game/game.component.ts
+++ Data/front/front/src/app/game/game.component.ts
@@ -22,4 +22,11 @@ export class GameComponent {
     { img:'/assets/img/profile.jpeg', name: 'eagoumi', rank: '/assets/icons/firstRank.svg'},
     { img:'/assets/img/profile.jpeg', name: 'imabid', rank: '/assets/icons/firstRank.svg'},
   ];
+  history = [
+    { img1:'/assets/img/profile.jpeg', img2:'/assets/img/profile.jpeg', names: 'kid-bouh vs yamzil', state: 'Win 🏆', date: 'Today'},
+    { img1:'/assets/img/profile.jpeg', img2:'/assets/img/profile.jpeg', names: 'kid-bouh vs agoumiiiiii', state: 'Win 🏆', date: 'Today'},
+    { img1:'/assets/img/profile.jpeg', img2:'/assets/img/profile.jpeg', names: 'kid-bouh vs simo', state: 'Lose 😥', date: 'Yesterday'},
+    { img1:'/assets/img/profile.jpeg', img2:'/assets/img/profile.jpeg', names: 'kid-bouh vs test', state: 'Lose 😥', date: 'Yesterday'},
+    { img1:'/assets/img/profile.jpeg', img2:'/assets/img/profile.jpeg', names: 'kid-bouh vs sel-mars', state: 'Win 🏆', date: 'Yesterday'},
+  ];
 }
